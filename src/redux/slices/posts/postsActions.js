487384dd2df6f8a525111f1d0d03d977dc03a23f
Artem <artem4rolov@gmail.com>
2023-02-05 14:58:45 +0300

    деплой 2

--- src/redux/slices/posts/postsActions.js
+++ src/redux/slices/posts/postsActions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // бэкенд по адресу
 // const backendURL = "http://localhost:5000";
-const backendURL = "https://nice-pink-lapel.cyclic.app/";
+const backendURL = process.env.REACT_APP_API_URL;
 
 // получить все посты
 export const getPosts = createAsyncThunk("posts/get", async () => {
